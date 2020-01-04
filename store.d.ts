b394ad3df01733d805ef1a854c1a256070638e6f
Brian Takita <brian.takita@gmail.com>
2020-01-04 17:22:34 -0500

    feat: store: b__ factories
    ∋ "@ctx-core/object": "^6.5.0"

--- store.d.ts
+++ store.d.ts
@@ -1,6 +1,14 @@
-export declare const __hostname: import("svelte/store").Writable<any>;
-export declare const __pathname: import("svelte/store").Writable<any>;
-export declare const __location__window: import("svelte/store").Writable<any>;
-export declare function reset__location__window(): void;
+import { Writable } from 'svelte/store';
+import { falsy } from '@ctx-core/function';
+export declare const b__hostname: (ctx?: any, opts?: any) => Writable<any>;
+export declare const __hostname: Writable<any>;
+export declare const b__pathname: (ctx?: any, opts?: any) => Writable<any>;
+export interface Writable__location__window extends Writable<falsy | Location> {
+    reset__location__window: () => void;
+}
+export declare const b__location__window: (ctx?: any, opts?: any) => Writable__location__window;
+export declare const __location__window: Writable__location__window;
+export declare const b__hostname__location__window: (ctx?: any, opts?: any) => import("svelte/store").Readable<any>;
 export declare const __hostname__location__window: import("svelte/store").Readable<any>;
+export declare const b__pathname__location__window: (ctx?: any, opts?: any) => import("svelte/store").Readable<any>;
 export declare const __pathname__location__window: import("svelte/store").Readable<any>;
--- store.ts
+++ store.ts
@@ -1,29 +1,51 @@
-import { writable, derived, get } from 'svelte/store'
+import { writable, derived, get, Writable } from 'svelte/store'
 import { _has__dom } from '@ctx-core/dom'
-export const __hostname = writable(null)
-export const __pathname = writable(null)
-const __bound__popstate__reload__location__window = writable(null)
-export const __location__window = writable(null)
-if (_has__dom()) {
-	reset__location__window()
+import { assign, _b } from '@ctx-core/object'
+import { falsy } from '@ctx-core/function'
+export const b__hostname = _b('__hostname', ()=>
+	writable(null))
+export const __hostname = b__hostname()
+export const b__pathname = _b('__pathname', ()=>
+	writable(null))
+export interface Writable__location__window extends Writable<falsy|Location> {
+	reset__location__window:()=>void
 }
-export function reset__location__window() {
-	if (!_has__dom()) return
-	if (!get(__bound__popstate__reload__location__window)) {
-		__bound__popstate__reload__location__window.set(true)
-		window.addEventListener('popstate', reset__location__window)
+export const b__location__window = _b<Writable__location__window>('__location__window', ()=>{
+	const __location__window = writable(null)
+	const __bound__popstate__reload__location__window = writable(null)
+	if (_has__dom()) {
+		reset__location__window()
 	}
-	__location__window.set(window.location)
-}
-export const __hostname__location__window = derived([
-		__hostname,
-		__location__window
-	],
-	([hostname, location__window]) =>
-		(location__window && location__window.hostname) || hostname || '')
-export const __pathname__location__window = derived([
-		__pathname,
-		__location__window
-	],
-	([pathname, location__window]) =>
-		(location__window && location__window.pathname) || pathname || '')
+	return assign(__location__window, {
+		reset__location__window,
+	})
+	function reset__location__window() {
+		if (!_has__dom()) return
+		if (!get(__bound__popstate__reload__location__window)) {
+			__bound__popstate__reload__location__window.set(true)
+			window.addEventListener(
+				'popstate',
+				reset__location__window)
+		}
+		__location__window.set(window.location)
+	}
+})
+export const __location__window = b__location__window()
+export const b__hostname__location__window = _b('__hostname__location__window', ctx=>
+	derived([
+			b__hostname(ctx),
+			b__location__window(ctx),
+		],
+		([hostname, location__window])=>
+			(location__window && location__window.hostname) || hostname || ''))
+export const __hostname__location__window = b__hostname__location__window()
+export const b__pathname__location__window = _b('__pathname__location__window', ctx=>
+	derived([
+			b__pathname(ctx),
+			b__location__window(ctx),
+		],
+		([pathname, location__window])=>
+			(location__window && location__window.pathname)
+			|| pathname
+			|| ''))
+export const __pathname__location__window = b__pathname__location__window()
